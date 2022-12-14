80cc455ecfd37ecae3ca6d367fbc16c66d973496
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2022-12-14 17:14:19 +0200

    costum packages

---
+++ client/t-shirts-app/src/components/Footer.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Typography, makeStyles, Container } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import Link from "@material-ui/core/Link";
+
+const useStyles = makeStyles((theme) => {
+  rootBox: {
+    justifyContent: "center";
+  }
+});
+
+const Footer = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <footer>
+        <Container maxWidth="lg">
+          <Box
+            py={6}
+            display="flex"
+            flexWrap="wrap"
+            alignItem="center"
+            className={classes.rootBox}
+          >
+            <Link href="#" color="inherit" underline="none">
+              {brand}
+            </Link>
+          </Box>
+        </Container>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
